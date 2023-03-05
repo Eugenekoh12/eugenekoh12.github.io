75002c501cba34c58b0d2da052c712f531f8c99a
Eugenekoh12 <ekoh16@yahoo.com.sg>
2023-03-05 15:32:51 +0800

    Update to 1.0.1 to add new userscript exclusion

--- userscripts/Replace Text (with personal exclusions).meta.js
+++ userscripts/Replace Text (with personal exclusions).meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Replace Text [picrew.me]
 // @namespace    https://github.com/Eugenekoh12/Personal
-// @version      1
+// @version      1.0.1
 // @description  Modified from https://github.com/erickRecai/Replace-Text v1.05.03 by guyRicky. Replaces text with other text.
 // @author       Eugenekoh12
 // @updateURL    https://github.com/Eugenekoh12/Personal/raw/master/userscripts/Replace%20Text%20(with%20personal%20exclusions).meta.js
