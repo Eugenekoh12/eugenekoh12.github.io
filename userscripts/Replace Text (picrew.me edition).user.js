2da85d551fd19fd462662e91b709ae6a60c56cda
Eugenekoh12 <ekoh16@yahoo.com.sg>
2023-03-05 14:18:06 +0800

    Update Replace Text (picrew.me edition).user.js

--- userscripts/Replace Text (picrew.me edition).user.js
+++ userscripts/Replace Text (picrew.me edition).user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Replace Text [picrew.me]
-// @namespace    https://github.com/Eugenekoh12/Personal/userscripts
+// @namespace    https://github.com/Eugenekoh12/Personal
 // @version      1
 // @description  Modified from https://github.com/erickRecai/Replace-Text v1.05.03 by guyRicky. Replaces text with other text for picrew.me only.
 // @author       Eugenekoh12
