6dd8ef0e329fc49800daeb31e45500c3e33060ea
Eugenekoh12 <ekoh16@yahoo.com.sg>
2023-03-05 14:22:35 +0800

    Create Replace Text (with personal exclusions).meta.js

---
+++ userscripts/Replace Text (with personal exclusions).meta.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name         Replace Text [picrew.me]
+// @namespace    https://github.com/Eugenekoh12/Personal
+// @version      1
+// @description  Modified from https://github.com/erickRecai/Replace-Text v1.05.03 by guyRicky. Replaces text with other text.
+// @author       Eugenekoh12
+// @updateURL    https://github.com/Eugenekoh12/Personal/raw/master/userscripts/Replace%20Text%20(with%20personal%20exclusions).meta.js
+// @downloadURL  https://github.com/Eugenekoh12/Personal/raw/master/userscripts/Replace%20Text%20(with%20personal%20exclusions).user.js
+// ==/UserScript==
