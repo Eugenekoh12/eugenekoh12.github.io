2a6866f82ede53ff4bb8d0cda4c9a6d4c799e6b2
Eugenekoh12 <ekoh16@yahoo.com.sg>
2023-03-05 15:56:34 +0800

    Update to 1.0.2.1
    Catch a unique area where the text has a stutter. Might have more minor updates as I read.

--- userscripts/Replace Text (AILRCIBTWII on whitemoonxblacksun.ca - novel reading edition).user.js
+++ userscripts/Replace Text (AILRCIBTWII on whitemoonxblacksun.ca - novel reading edition).user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Replace Text [whitemoonxblacksun.ca - AILRCIBTWII]
 // @namespace    https://github.com/Eugenekoh12/Personal
-// @version      1.0.2
+// @version      1.0.2.1
 // @description  Modified from https://github.com/erickRecai/Replace-Text v1.05.03 by guyRicky. Replaces text with other text for picrew.me only.
 // @author       Eugenekoh12
 
@@ -113,6 +113,7 @@ DB. support functions
         let replaceRules = [
             //[//i, ""], // rule template
 
+            [/Da, Dahei/i, " Coaly"],
             [/ Dahei /i, " Coaly "],
             [/Dahei /i, "Coaly "],
             [/ Dahei/i, " Coaly"],
