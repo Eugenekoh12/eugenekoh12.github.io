4b0aa454d2aa9c420f315bba55c9d51bc2a8a193
Eugenekoh12 <ekoh16@yahoo.com.sg>
2023-03-05 15:46:00 +0800

    Update to 1.0.2
    Minor update!

--- userscripts/Replace Text (AILRCIBTWII on whitemoonxblacksun.ca - novel reading edition).meta.js
+++ userscripts/Replace Text (AILRCIBTWII on whitemoonxblacksun.ca - novel reading edition).meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Replace Text [whitemoonxblacksun.ca - AILRCIBTWII]
 // @namespace    https://github.com/Eugenekoh12/Personal
-// @version      1.0.1
+// @version      1.0.2
 // @description  Modified from https://github.com/erickRecai/Replace-Text v1.05.03 by guyRicky. Replaces text with other text.
 // @author       Eugenekoh12
 // @updateURL    https://github.com/Eugenekoh12/Personal/raw/master/userscripts/Replace%20Text%20(AILRCIBTWII%20on%20whitemoonxblacksun.ca%20-%20novel%20reading%20edition).meta.js
