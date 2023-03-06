fb1e32738c077760e22958271cfc8156d7d704d5
Eugenekoh12 <ekoh16@yahoo.com.sg>
2023-03-06 18:21:43 +0800

    Update to 1.0.3.4

--- userscripts/Replace Text/Websites/Light Novels & Stories/AILRCIBTWII on whitemoonxblacksun.ca.meta.js
+++ userscripts/Replace Text/Websites/Light Novels & Stories/AILRCIBTWII on whitemoonxblacksun.ca.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Replace Text - whitemoonxblacksun.ca - AILRCIBTWII
 // @namespace    https://github.com/Eugenekoh12/Personal
-// @version      1.0.3.3
+// @version      1.0.3.4
 // @description  Modified from https://github.com/erickRecai/Replace-Text v1.05.03 by guyRicky. Replaces text with other text for AILRCIBTWII on whitemoonxblacksun.ca.
 // @author       Eugenekoh12
 // @updateURL    https://github.com/Eugenekoh12/Personal/raw/master/userscripts/Replace%20Text/Websites/Light%20Novels%20%26%20Stories/AILRCIBTWII%20on%20whitemoonxblacksun.ca.meta.js
