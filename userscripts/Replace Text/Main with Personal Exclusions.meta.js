6655cb95dadb9242beab794327543863937b70e0
Eugenekoh12 <ekoh16@yahoo.com.sg>
2023-03-06 08:23:36 +0800

    New Meta Location for Main with Personal Exclusions
    New meta file to update in the future.

---
+++ userscripts/Replace Text/Main with Personal Exclusions.meta.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name         Replace Text [with personal exclusions]
+// @namespace    https://github.com/Eugenekoh12/Personal
+// @version      1.0.3
+// @description  Modified from https://github.com/erickRecai/Replace-Text v1.05.03 by guyRicky. Replaces text with other text.
+// @author       Eugenekoh12
+// @updateURL    https://github.com/Eugenekoh12/Personal/raw/master/userscripts/Replace%20Text/Main%20with%20Personal%20Exclusions.meta.js
+// @downloadURL  https://github.com/Eugenekoh12/Personal/raw/master/userscripts/Replace%20Text/Main%20with%20Personal%20Exclusions.user.js
+// ==/UserScript==
