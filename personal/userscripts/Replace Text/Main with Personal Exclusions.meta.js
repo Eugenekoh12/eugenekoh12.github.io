af0d7bcc5b8b943945abe2887599836f4188a465
Eugenekoh12 <ekoh16@yahoo.com.sg>
2023-10-10 06:56:36 +0800

    Update Main with Personal Exclusions.meta.js
    Preparing to Move & Archive

--- personal/userscripts/Replace Text/Main with Personal Exclusions.meta.js
+++ personal/userscripts/Replace Text/Main with Personal Exclusions.meta.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         Replace Text Main - with Personal Exclusions
 // @namespace    https://github.com/Eugenekoh12/Personal
-// @version      1.0.3
+// @version      1.0.4
 // @description  Modified from https://github.com/erickRecai/Replace-Text v1.05.03 by guyRicky. Replaces text with other text.
 // @author       Eugenekoh12
-// @updateURL    https://github.com/Eugenekoh12/Personal/raw/master/userscripts/Replace%20Text/Main%20with%20Personal%20Exclusions.meta.js
-// @downloadURL  https://github.com/Eugenekoh12/Personal/raw/master/userscripts/Replace%20Text/Main%20with%20Personal%20Exclusions.user.js
+// @updateURL    https://eugenekoh12.github.io/personal/userscripts/Replace%20Text/Main%20with%20Personal%20Exclusions.meta.js
+// @downloadURL  https://eugenekoh12.github.io/personal/userscripts/Replace%20Text/Main%20with%20Personal%20Exclusions.user.js
 // ==/UserScript==
