b723a2321e20d7e4ac37e49f2b5dfa8efe1a3788
Eugenekoh12 <ekoh16@yahoo.com.sg>
2023-10-10 06:57:44 +0800

    Update picrew.me.meta.js
    Preparing to Move & Archive

--- personal/userscripts/Replace Text/Websites/picrew.me.meta.js
+++ personal/userscripts/Replace Text/Websites/picrew.me.meta.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         Replace Text - picrew.me specific
 // @namespace    https://github.com/Eugenekoh12/Personal
-// @version      1.0.1
+// @version      1.0.2
 // @description  Modified from https://github.com/erickRecai/Replace-Text v1.05.03 by guyRicky. Replaces text with other text for picrew.me only.
 // @author       Eugenekoh12
-// @updateURL    https://github.com/Eugenekoh12/Personal/raw/master/userscripts/Replace%20Text/Websites/picrew.me.meta.js
-// @downloadURL  https://github.com/Eugenekoh12/Personal/raw/master/userscripts/Replace%20Text/Websites/picrew.me.user.js
+// @updateURL    https://eugenekoh12.github.io/personal/userscripts/Replace%20Text/Websites/picrew.me.meta.js
+// @downloadURL  https://eugenekoh12.github.io/personal/userscripts/Replace%20Text/Websites/picrew.me.user.js
 // ==/UserScript==
